1c1315e38c471ecaf494eee13880aeb2ce7d6c70
krego <mateusz.olczynski@gmail.com>
2017-02-10 18:00:17 +0100

    Change css class field to event type

--- django_bootstrap_calendar/models.py
+++ django_bootstrap_calendar/models.py
@@ -3,14 +3,14 @@ __author__ = 'sandlbn'
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from utils import datetime_to_timestamp
+from .utils import datetime_to_timestamp
 
 
 class CalendarEvent(models.Model):
     """
     Calendar Events
     """
-    CSS_CLASS_CHOICES = (
+    EVENT_TYPE_CHOICES = (
         ('', _('Normal')),
         ('event-warning', _('Warning')),
         ('event-info', _('Info')),
@@ -21,8 +21,8 @@ class CalendarEvent(models.Model):
     )
     title = models.CharField(max_length=255, verbose_name=_('Title'))
     url = models.URLField(verbose_name=_('URL'), null=True, blank=True)
-    css_class = models.CharField(blank=True, max_length=20, verbose_name=_('CSS Class'),
-                                 choices=CSS_CLASS_CHOICES)
+    type = models.CharField(blank=True, max_length=20, verbose_name=_('CSS Class'),
+                            choices=EVENT_TYPE_CHOICES)
     start = models.DateTimeField(verbose_name=_('Start Date'))
     end = models.DateTimeField(verbose_name=_('End Date'), null=True,
                                blank=True)
