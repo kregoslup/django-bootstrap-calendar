e35b35e720ed0787e6a34c4461cb1550f694498c
krego <mateusz.olczynski@gmail.com>
2017-02-10 18:13:38 +0100

    Add validating date

--- django_bootstrap_calendar/serializers.py
+++ django_bootstrap_calendar/serializers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.db.models.query_utils import Q
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from django_bootstrap_calendar.models import CalendarEvent
 
@@ -12,3 +14,14 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = CalendarEvent
         fields = ('hash', 'title', 'url', 'type', 'start', 'end', 'length')
+
+    def date_clashes(self, start, end):
+        return CalendarEvent.objects.filter(
+            Q(start__range=(start, end) | Q(end__range=(start, end)))).exists()
+
+    def validate(self, attrs):
+        start = attrs.get('start', None)
+        end = attrs.get('end', None)
+        if start and end and self.date_clashes(start, end):
+            raise ValidationError({'Invalid date'})
+        return super(EventSerializer, self).validate(attrs)
